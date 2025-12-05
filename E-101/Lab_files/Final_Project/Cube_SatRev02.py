a4a26dd9e705b4945864331f0075bf477665682a
NINJADRAGON32 <102389539+NINJADRAGON32@users.noreply.github.com>
2025-12-05 15:45:18 -0500

    s

--- E-101/Lab_files/Final_Project/Cube_SatRev02.py
+++ E-101/Lab_files/Final_Project/Cube_SatRev02.py
@@ -1,10 +1,10 @@
-# Revision 1 of the Cube sat programing for the raspberry pi.
+# Revision 2 of the Cube sat programing for the raspberry pi.
 # these are the basic functions of the raspberry pi that will later allow the pi to be a crucial part of the game 
 
 # Imports and Globals
 import random as r
 from sense_hat import SenseHat
-sense = SenseHat
+sense = SenseHat()
 from time import sleep
 # ________________________________________________________________________________________
 players = []
@@ -123,30 +123,32 @@ def grabbing_players():
     num_players = 1
     sense.show_message("How many players!!", scroll_speed = 0.05,text_colour = [139,0,0], back_colour = [0,0,0])
     event = sense.stick.wait_for_event()
-    while event.direction != 'middle' and event.action != 'pressed':
-        if num_players==1:
+    while not (event.direction == 'middle' and event.action == 'pressed'):
+        sleep(1)
+        if num_players == 1:
             sense.set_pixels(one)
-        elif num_players==2:
+        elif num_players == 2:
             sense.set_pixels(two)
-        elif num_players==3:
+        elif num_players == 3:
             sense.set_pixels(three)
-        elif num_players==4:
+        elif num_players == 4:
             sense.set_pixels(four)
-        if event.direction == 'up':
-            if num_players == 4:
-                num_players=1
-            else:
-                num_players+=1
-        elif event.direction =='down':
-            if num_players == 1:
-                num_players=4
-            else: 
-                num_players-=1
 
-def turn():
+        event = sense.stick.wait_for_event()  
+        if event.direction == 'up':
+            num_players = 1 if num_players == 4 else num_players + 1
+        elif event.direction == 'down':
+            num_players = 4 if num_players == 1 else num_players - 1
+    return (num_players)
+
+def take_turn(p):
     # this funtion programs the turn order per player
+    event = sense.stick.wait_for_event()
+    while not (event.direction == 'middle' and event.action == 'pressed'):
+        sense.show_message(f"Player {p}'s turn  ", scroll_speed = 0.05,text_colour = [139,0,0], back_colour = [0,0,0])
     roll = roll_dice()
     C = assign_color()
+    sense.clear()
     sense.set_pixels([
     C,C,C,C,C,C,C,C,
     C,C,C,C,C,C,C,C,
@@ -158,6 +160,7 @@ def turn():
     C,C,C,C,C,C,C,C
     ])
     sleep(4)
+    sense.clear()
     if roll==1:
         sense.set_pixels(one)
     elif roll==2:
@@ -174,32 +177,48 @@ def turn():
         sense.set_pixels(seven)
     elif roll==8:
         sense.set_pixels(eight)
-    
     return
 
 # main ---------------------------------------------------------------------------------
-
 grabbing_players()
-t_o = assign_turn_order(players)
-print(f"Here is our turn order: {t_o}")
-print(f"Starting game!!!!")
+sense.show_message("Starting Game", scroll_speed = 0.05,text_colour = [139,0,0], back_colour = [0,0,0])
 gamestate=1
-while gamestate==1:
-    print(f"it's {t_o[index]}'s turn")
-    ui = str(input("press (r) to roll the dice and get your card color. "))
-    roll = roll_dice()
-    print(f"you rolled a {roll} and your card color is {assign_color()}")
-    ui = str(input("Did you get the question right?(y/n): "))
-    if ui =="y":
-        print(f"AWESOME move forward {roll} spaces")
-    else: 
-        print("you suck haha loserrrr!!!!!!!!!!!")
-    if index == (len(t_o)-1):
-        index = 0
-    else:
-        index = index+1
-    ui = str(input("did you win?(y/n): "))
-    if ui == "y":
-        gamestate=0
+p = 1
+turn = 0
+while gamestate == 1:
+    take_turn(p)
+    if turn>=5:
+        sense.show_message("Did u win? up-yes down-no", scroll_speed = 0.05,text_colour = [139,0,0], back_colour = [0,0,0])
+        event = sense.stick.wait_for_event()
+        if event.direction == 'up': 
+            gamestate == 0
+
+    
+
+
+
+
+# grabbing_players()
+# t_o = assign_turn_order(players)
+# print(f"Here is our turn order: {t_o}")
+# print(f"Starting game!!!!")
+# gamestate=1
+# while gamestate==1:
+#     print(f"it's {t_o[index]}'s turn")
+#     ui = str(input("press (r) to roll the dice and get your card color. "))
+#     roll = roll_dice()
+#     print(f"you rolled a {roll} and your card color is {assign_color()}")
+#     ui = str(input("Did you get the question right?(y/n): "))
+#     if ui =="y":
+#         print(f"AWESOME move forward {roll} spaces")
+#     else: 
+#         print("you suck haha loserrrr!!!!!!!!!!!")
+#     if index == (len(t_o)-1):
+#         index = 0
+#     else:
+#         index = index+1
+#     ui = str(input("did you win?(y/n): "))
+#     if ui == "y":
+#         gamestate=0
 
 
